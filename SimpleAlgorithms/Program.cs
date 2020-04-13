947f88af113e4cb74a65dd24fb925f0552501a0a
Kirill.Cheremisin <cheremisin_k@mail.ru>
2020-04-13 20:59:47 +0300

    check doubly linked list

--- SimpleAlgorithms/Program.cs
+++ SimpleAlgorithms/Program.cs
@@ -10,7 +10,71 @@ namespace SimpleAlgorithms
     {
         static void Main(string[] args)
         {
-            CheckLinkedList();
+            //CheckLinkedList();
+            CheckDoublyLinkedList();
+        }
+
+        public static void CheckDoublyLinkedList()
+        {
+            DoublyLinkedList<string> nameList = new DoublyLinkedList<string>();
+            nameList.Add("Tom");
+            nameList.Add("Kirill");
+            nameList.Add("Maria");
+            nameList.Add("Ivan");
+
+            Console.WriteLine("Write initial name list");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Write name list back direction");
+            foreach(var name in nameList.BackEnumerator())
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Remove Kirill and write name list");
+            nameList.Remove("Kirill");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Remove Tom and write name list");
+            nameList.Remove("Tom");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Check if Tom exist");
+            bool isPresent = nameList.Contains("Tom");
+            Console.WriteLine(isPresent ? "Tom Yes" : "Tom No");
+
+            Console.WriteLine("");
+            Console.WriteLine("Check if Ivan exist");
+            isPresent = nameList.Contains("Ivan");
+            Console.WriteLine(isPresent ? "Ivan Yes" : "Ivan No");
+
+            Console.WriteLine("");
+            Console.WriteLine("Append Artem");
+            Console.WriteLine("Check if Artem exist");
+            nameList.AddFirst("Artem");
+            isPresent = nameList.Contains("Artem");
+            Console.WriteLine(isPresent ? "Artem Yes" : "Artem No");
+
+            Console.WriteLine("");
+            Console.WriteLine("Count is {0}", nameList.Count);
+            Console.WriteLine("Clear list");
+            nameList.Clear();
+            Console.WriteLine("Count is {0}", nameList.Count);
+            Console.WriteLine("Is list empty {0}", nameList.IsEmpty.ToString());
+            _ = Console.ReadLine();
         }
 
         private static void CheckLinkedList()
