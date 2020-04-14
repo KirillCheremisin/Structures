07ba80804da7e7177c762153f48f031f590f2481
Kirill.Cheremisin <cheremisin_k@mail.ru>
2020-04-14 23:28:35 +0300

    test class LinkedListTest, test method Add

---
+++ SimpleAlgorithmsTests/LinkedListTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleAlgorithmsTests
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            // [given] empty linked list
+            // [given] text value "x"
+            var myList = new SimpleAlgorithms.LinkedList<string>();
+            string myString = DateTime.Today.ToString();
+
+            // [when] add the value "X"
+            myList.Add(myString);
+
+            // [then] Count is 1
+            Assert.AreEqual(1, myList.Count);
+
+            // [then] value is "X"
+            var item = myList.GetEnumerator();
+            item.MoveNext();
+            Assert.AreEqual(myString, item.Current);
+        }
+        [TestMethod]
+        public void Remove()
+        {
+
+        }
+    }
+}
---
+++ SimpleAlgorithmsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleAlgorithmsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleAlgorithmsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1759ddd-ed7a-4e28-8ba7-4c8aaeb7164c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
